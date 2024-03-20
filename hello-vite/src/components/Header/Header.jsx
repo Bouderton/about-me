d69658e526c00dd3d4c8446f370978840fc474cc
bouderton <rmkhazal@gmail.com>
2024-03-20 14:56:03 -0700

    added nav

--- hello-vite/src/components/Header/Header.jsx
+++ hello-vite/src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import "../Header/Header.css";
 function Header() {
   return (
     <header className="header">
-      <div className="header__header"></div>
+      <div className="header__header">
+        <p>Header</p>
+      </div>
     </header>
   );
 }
