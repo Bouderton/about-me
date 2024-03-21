c42a046b97f1260aef2278c4bf53d544fecab8ae
bouderton <rmkhazal@gmail.com>
2024-03-20 22:03:57 -0700

    nav setup

--- hello-vite/src/components/App/App.jsx
+++ hello-vite/src/components/App/App.jsx
@@ -3,14 +3,16 @@ import { useState } from "react";
 // import viteLogo from "./vite.svg";
 import "../../../src/index.css";
 import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <>
       <div className="page">
+      <Nav/>
       <Header />
-      <h1>Hello</h1>
+      <h1>Content</h1>
       <Footer/>
       </div>
     </>
---
+++ hello-vite/src/components/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import "./Nav.css";
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <ul className="nav__list">
+                <li className="nav__list-item">Home</li>
+                <li className="nav__list-item">Skills</li>
+                <li className="nav__list-item">My Story</li>
+                <li className="nav__list-item">Contact Me</li>
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
