5e6c04f6a342c90b707c4c7392beff3bb9fb5b5a
bouderton <rmkhazal@gmail.com>
2024-03-21 23:19:32 -0700

    starting to style nav:

--- hello-vite/src/components/Nav/Nav.jsx
+++ hello-vite/src/components/Nav/Nav.jsx
@@ -1,16 +1,25 @@
+import React from "react";
 import "./Nav.css";
+import github from "../../images/github.png";
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <ul className="nav__list">
-        <li className="nav__list-item">Home</li>
-        <li className="nav__list-item">Skills</li>
-        <li className="nav__list-item">My Story</li>
-        <li className="nav__list-item">Projects</li>
-        <li className="nav__list-item">Contact Me</li>
-      </ul>
-    </nav>
+    <div className="nav__container">
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__list-item">Home</li>
+          <li className="nav__list-item">Skills</li>
+          <li className="nav__list-item">My Story</li>
+          <li className="nav__list-item">Projects</li>
+          <li className="nav__list-item">Contact Me</li>
+        </ul>
+      </nav>
+      <div className="nav__links">
+        <img className="nav__links-image" src={github} alt="github logo" />
+        <img className="nav__links-image" src={github} alt="github logo" />
+        <img className="nav__links-image" src={github} alt="github logo" />
+      </div>
+    </div>
   );
 };
 
