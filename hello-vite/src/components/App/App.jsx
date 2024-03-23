fb1ae4f197e5ae9fed6d5ba9c0a9a776bdc8f897
bouderton <rmkhazal@gmail.com>
2024-03-22 22:09:14 -0700

    added browser router:

--- hello-vite/src/components/App/App.jsx
+++ hello-vite/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 // import reactLogo from "../assets/react.svg";
 // import viteLogo from "./vite.svg";
+import { Routes, Route } from "react-router-dom";
 import "../../../src/index.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
