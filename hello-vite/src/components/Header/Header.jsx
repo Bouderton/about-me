3e907f5a4652abb85e1fdc5c4f27ddde240a67e2
bouderton <rmkhazal@gmail.com>
2024-04-14 10:32:26 -0700

    new animation background

--- hello-vite/src/components/Header/Header.jsx
+++ hello-vite/src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import Nav from "../Nav/Nav";
 import "../Header/Header.css";
-import looped_anim2 from "../../images/looped_anim2.mp4";
+import looped_anim3 from "../../images/looped_anim3.mp4";
 
 function Header() {
   return (
     <>
       <header className="header">
-        <video className="animation" src={looped_anim2} autoPlay loop muted />
+        <video className="animation" src={looped_anim3} autoPlay loop muted />
         <div className="header__text">
           <h1 className="header__title">Ryan Khazal</h1>
           <p className="header__subtitle">
