f761ca5090d992b281b7c8657415f77476955f44
bouderton <rmkhazal@gmail.com>
2024-04-15 12:35:15 -0700

    logos

--- hello-vite/src/components/Footer/Footer.jsx
+++ hello-vite/src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import "./Footer.css";
+import github from "../../images/github.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer__text">&copy; Ryan Khazal 2024</p>
+      {/* <button type="button">
+      <img className='footer__github' src={github} alt="github logo"/>
+      </button> */}
     </footer>
   );
 };
