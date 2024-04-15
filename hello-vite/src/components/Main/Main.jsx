889db10084e62be2bd0e8e60507f94de32675925
bouderton <rmkhazal@gmail.com>
2024-04-15 11:09:31 -0700

    entry button

--- hello-vite/src/components/Main/Main.jsx
+++ hello-vite/src/components/Main/Main.jsx
@@ -6,11 +6,12 @@ import looped_anim4 from "../../images/looped_anim4.mp4";
 const Main = () => {
   return (
       <main className="main">
-        <div className="main__text">
+        <div className="main__content">
           <h1 className="main__title">Ryan Khazal</h1>
           <p className="main__subtitle">
             Full-Stack Web Developer & 3D Artist
           </p>
+          <button type='text' className='main__entry-button'>Who Am I?</button>
         </div>
         <Video vid={looped_anim4}/>
       </main>
