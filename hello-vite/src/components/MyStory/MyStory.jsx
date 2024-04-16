4b6c7091e71296070877e2f2cb566640b6b1b2b5
bouderton <rmkhazal@gmail.com>
2024-04-15 18:37:58 -0700

    text setup 1

--- hello-vite/src/components/MyStory/MyStory.jsx
+++ hello-vite/src/components/MyStory/MyStory.jsx
@@ -7,7 +7,10 @@ const MyStory = () => {
     <>
       <Nav/>
     <div className="my-story">
+      <div className="my-story__text-container">
       <h2 className="my-story__title">Who Am I?</h2>
+      <p className="my-story__paragraph"> THIS IS A TEST PARAGRAPH As the sun dipped below the horizon, casting a warm glow across the tranquil meadow, a lone figure sat beneath the sprawling branches of an ancient oak tree. With a worn leather journal resting on their lap and a pen poised between their fingers, they lost themselves in the rhythm of their thoughts. Each stroke of the pen across the weathered pages seemed to breathe life into the stories that danced within their mind. The breeze whispered secrets through the rustling leaves, carrying with it the scent of wildflowers and the promise of new beginnings. In that fleeting moment, amidst the symphony of nature's serenade, they found solace in the simple act of creation, weaving words into a tapestry of imagination and wonder.</p>
+    </div>
     </div>
       <Footer/>
     </>
