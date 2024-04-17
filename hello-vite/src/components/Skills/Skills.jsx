3aaa8ced48e042497a8a86a150bb181fc78b737d
bouderton <rmkhazal@gmail.com>
2024-04-16 21:33:26 -0700

    skills page setup 1

--- hello-vite/src/components/Skills/Skills.jsx
+++ hello-vite/src/components/Skills/Skills.jsx
@@ -1,18 +1,16 @@
 import "./Skills.css";
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
-import woowoo4k from "../../images/woowoo4k.mp4";
+import Video from "../Video/Video";
+import woowoo from "../../images/woowoo4k.mp4";
 
 const Skills = () => {
   return (
     <>
       <Nav/>
-        <div className="skills__background">
-        <video className="animation" src={woowoo4k} autoPlay loop muted />
-        <div className='text__container'>
-        <h2 className='skills__title'>What I Do</h2>
-          <p className="text">Buncha Shit Lolasdfhasdfhasdfhasdfh</p>
-        </div>
+        <div className="skills">
+          <h2 className='skills__title'>What I Do</h2>
+          <Video vid={woowoo}/>
         </div>
       <Footer/>
     </>
