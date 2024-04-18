c7170ef89684ac714ec8d5d2c885bef48c0254ea
bouderton <rmkhazal@gmail.com>
2024-04-18 12:12:47 -0700

    more testing

--- hello-vite/src/components/Skills/Skills.jsx
+++ hello-vite/src/components/Skills/Skills.jsx
@@ -6,12 +6,12 @@ const Skills = () => {
     <>
         <div className="skills">
             <Header text={"What I Do"} />
+          <div className="box">
+            <h2>TESTING</h2>
           <div className="skills__component">
             <h3 className='skills__component-title'>JavaScript</h3>
             <p className='skills__component-text'>Blhablhablhablhab</p>
           </div>
-          <div className="box">
-            <h2>TESTING</h2>
           </div>
         </div>
     </>
