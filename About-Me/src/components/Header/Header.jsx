8c688508d342ffc23a0f2aca0c7d547513ea4abe
bouderton <rmkhazal@gmail.com>
2024-05-01 07:29:29 -0700

    titles added

--- About-Me/src/components/Header/Header.jsx
+++ About-Me/src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className="header__container">
       <header className="header">
-        <h1>HELLO?</h1>
+        <h1 className="header__title">Ryan Khazal</h1>
+        <h2 className="header__subtitle">Full-Stack Web Developer</h2>
+        <h2 className="header__subtitle">3D Artist</h2>
         <img src={logo} alt="logo" className="header__logo" />
       </header>
     </div>
