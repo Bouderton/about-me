07edb4cad2efd1882a6cbe528e5d36b8aeb86e86
bouderton <rmkhazal@gmail.com>
2024-05-01 08:31:13 -0700

    about me section

--- About-Me/src/components/About/About.jsx
+++ About-Me/src/components/About/About.jsx
@@ -4,7 +4,13 @@ const About = () => {
   return (
     <div className="about__container">
       <h2 className="about__title">About Me</h2>
-      <p className="about__text"></p>
+      <p className="about__text">
+        Random bullshit to get hired Random bullshit to get hiredRandom bullshit
+        to get hiredRandom bullshit to get hiredRandom bullshit to get
+        hiredRandom bullshit to get hiredRandom bullshit to get hiredRandom
+        bullshit to get hiredRandom bullshit to get hiredRandom bullshit to get
+        hiredRandom bullshit to get hiredRandom bullshit to get hired :3
+      </p>
     </div>
   );
 };
