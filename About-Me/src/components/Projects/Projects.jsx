08a0e60e013b7b810239452bea9db702fd450749
bouderton <rmkhazal@gmail.com>
2024-06-16 00:09:32 -0700

    Projects component

---
+++ About-Me/src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div className="projects__container">
+      <div className="projects"></div>
+    </div>
+  );
+};
+
+export default Projects;
