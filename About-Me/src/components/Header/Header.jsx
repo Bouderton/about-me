2af4a433ecf55887c89a5d9adfce44e489d582f3
bouderton <rmkhazal@gmail.com>
2024-06-20 13:16:37 -0700

    new logo

--- About-Me/src/components/Header/Header.jsx
+++ About-Me/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import logo from "../../images/Logo.svg";
+import rklogo from "../../images/rklogo.svg";
 import linkedin from "../../images/linkedin.svg";
 import github from "../../images/GitHub.svg";
 
@@ -10,10 +10,11 @@ const Header = () => {
         <h2 className="header__title">Ryan Khazal</h2>
         <p className="header__subtitle">Full-Stack Web Developer</p>
         <p className="header__subtitle">3D Artist</p>
-        <img src={logo} alt="logo" className="header__logo" />
+        <img src={rklogo} alt="logo" className="header__logo" />
         <img src={linkedin} alt="linkedin" className="header__linkedin" />
         <img src={github} alt="github" className="header__github" />
         {/* FIX BLURRY LOGOS */}
+        {/* I think they're fixed lol */}
       </header>
     </div>
   );
