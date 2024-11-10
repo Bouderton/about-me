46cd6d42ac63fb6c918007da71a3c584639be881
bouderton <rmkhazal@gmail.com>
2024-11-10 11:37:33 -0800

    new about section savepoint + text changes

--- About-Me/src/components/About/About.jsx
+++ About-Me/src/components/About/About.jsx
@@ -1,29 +1,31 @@
 import "./About.css";
 // import Break from "../Break/Break";
+import me from "../../images/me.jpeg";
 
 const About = () => {
   return (
     <>
-      <div className="about__container">
+      <section className="about">
         <h2 className="about__title">About Me</h2>
-        {/* I GOTTA CHANGE THIS LOL */}
-        <p className="about__text" style={{ textAlign: "left" }}>
-          I am a Full-Stack Web Developer looking for new opportunities in the
-          tech world. I graduated from one of the top Software Engineering Boot
-          Camps in the U.S, with internship experience and over 1,200
-          contributions on GitHub in the last year. I specialize in creating
-          responsive and intuitive web applications fit for all devices using
-          React, JavaScript, HTML/CSS, Express, Node and MongoDB.
-        </p>
-        <p className="about__text" style={{ textAlign: "right" }}>
-          As a former Student Athlete with a Regional and State Championship;
-          Resilience, Teamwork and Communication are customary in my love for
-          innovative technology. I am confident that my background in athletics,
-          paired with my passion for health and fitness, will allow me to thrive
-          in a working environment that shares my drive to change the world
-          through health and technology.
-        </p>
-      </div>
+        <div className="about__content">
+          <div className="about__text-container">
+            <p className="about__text" style={{ textAlign: "left" }}>
+              // I graduated from one of the top Software Engineering Boot Camps
+              in the U.S, with over 1,200 contributions on GitHub in the last
+              year.
+            </p>
+            <p className="about__text">
+              As a former Student Athlete with a Regional and State
+              Championship; Resilience, Teamwork and Communication are customary
+              in my love for innovative technology. I am confident that my
+              background in athletics, paired with my passion for health and
+              fitness, will allow me to thrive in a working environment that
+              shares my drive to change the world through health and technology.
+            </p>
+          </div>
+          <img className="about__img" src={me} alt="Ryan Khazal Photo" />
+        </div>
+      </section>
     </>
   );
 };
--- About-Me/src/components/Header/Header.jsx
+++ About-Me/src/components/Header/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
       <Navigation />
       <header className="header">
         <div className="header__content">
-          <h2 className="header__title">/ Ryan Khazal</h2>
-          <p className="header__subtitle">/ Full-Stack Software Engineer</p>
+          <h2 className="header__title">Ryan Khazal</h2>
+          <p className="header__subtitle">// Full-Stack Software Engineer</p>
           <img src={rklogo} alt="logo" className="header__logo" />
           <p className="header__text">
-            / I am a Full-Stack Web Developer from Las Vegas, Nevada looking for
-            new opportunities in the tech world. I specialize in creating
+            // I am a Full-Stack Web Developer from Las Vegas, Nevada looking
+            for new opportunities in the tech world. I specialize in creating
             responsive and intuitive web applications fit for all devices.
           </p>
         </div>
