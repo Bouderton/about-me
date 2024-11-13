f7e2c9b7401e86ee71fe69f5d5666219425ba9b3
bouderton <rmkhazal@gmail.com>
2024-11-13 11:36:26 -0800

    add-motion-css

--- About-Me/src/components/Motion/Motion.jsx
+++ About-Me/src/components/Motion/Motion.jsx
@@ -9,11 +9,26 @@ const Motion = () => {
         display: "grid",
         justifyContent: "center",
         height: "500px",
-        // gap: "0.8rem",
       }}
     >
       <h1 style={{ fontSize: "60px" }}>LEARNING MOTION</h1>
-      <motion.button className="mobutton">Click Me!</motion.button>
+      <MotionConfig transition={{ duration: 0.125, ease: "easeInOut" }}>
+        <motion.button
+          className="mobutton"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          Click Me!
+        </motion.button>
+        <motion.button
+          style={{ background: "red" }}
+          className="mobutton"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          No Me!
+        </motion.button>
+      </MotionConfig>
     </div>
   );
 };
