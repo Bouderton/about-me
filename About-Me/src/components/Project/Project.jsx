d823f05d83533ecacf102bbe8c7e9ecad872b71f
bouderton <rmkhazal@gmail.com>
2024-11-14 11:47:54 -0800

    add-top-project-cards

--- About-Me/src/components/Project/Project.jsx
+++ About-Me/src/components/Project/Project.jsx
@@ -2,7 +2,7 @@ import "./Project.css";
 import { useState } from "react";
 import github from "../../images/github.svg";
 
-const Project = ({ link, project, title, live, text, repo }) => {
+const Project = ({ link, img, title, live, text, repo }) => {
   // use live state to determine a live link to the project
   // Hover on project turns the card and github logo #3a85ff
   return (
@@ -10,14 +10,11 @@ const Project = ({ link, project, title, live, text, repo }) => {
       <div className="project">
         <div className="project__content">
           <h3 className="project__title">{title}</h3>
-          <a
-            href={link}
-            rel="noopener noreferer"
-            target="_blank"
-            // style={{ width: "100%", height: "auto" }}
-          >
-            <img src={project} className="project__img" alt="Project Image" />
-          </a>
+          {live ? (
+            <a href={link} rel="noopener noreferer" target="_blank">
+              <img src={img} className="project__img" alt="Project Image" />
+            </a>
+          ) : null}
           <div className="project__description">
             <p className="project__text">{text}</p>
           </div>
--- About-Me/src/components/Projects/Projects.jsx
+++ About-Me/src/components/Projects/Projects.jsx
@@ -2,8 +2,8 @@ import "./Projects.css";
 import "../Project/Project";
 import Project from "../Project/Project";
 import NewsExplorerLive from "../../images/NewsExplorerLive.png";
-// import wtwr from "../../images/wtwr.png";
-// import aroundtheus from "../../images/aroundtheus.png";
+import wtwr from "../../images/wtwr.png";
+import aroundtheus from "../../images/aroundtheus.png";
 
 const Projects = () => {
   return (
@@ -11,23 +11,34 @@ const Projects = () => {
       <section className="projects">
         <h2 className="projects__title">What I've Done</h2>
         <div className="projects__content">
-          {/* <div className="project__title-container"></div> */}
           <Project
             repo="https://github.com/Bouderton/news_explorer_frontend"
             link="https://newsexplorer.utdnews.com/"
-            project={NewsExplorerLive}
+            img={NewsExplorerLive}
+            live={true}
             title="News Explorer"
-            text="NewsExplorer is a dynamic Full Stack Web Application designed to provide users with a seamless way to explore, search, and save the latest news articles."
-            // Powered by an open-source news API, the platform delivers real-time updates on trending topics from around the world.
+            text="NewsExplorer is a dynamic Full Stack Web Application 
+            designed to provide users with a seamless way to explore, search, 
+            and save the latest news articles."
           />
           <Project
-            // project={wtwr}
+            img={wtwr}
+            live={true}
             title="What to Wear"
             link="https://wtwr.mindhackers.org/"
+            repo="https://github.com/Bouderton/se_project_react"
+            text="What To Wear is a responsive Full Stack Web Application
+            that fetches realtime location and weather data to suggest the user
+            clothing items fit for the weather outside."
           />
           <Project
+            repo="https://github.com/Bouderton/se_project_aroundtheus"
             title="Around The US"
+            live={true}
+            img={aroundtheus}
             link="https://bouderton.github.io/se_project_aroundtheus/"
+            text="This Instagram look-alike is a Front-End social media page
+            on the Worldwide Explorer - Jacques Cousteau."
           />
           {/* Around the US*/}
           {/* Slopopedia */}
