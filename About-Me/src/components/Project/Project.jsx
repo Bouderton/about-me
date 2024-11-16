b3e0b4a56a16b8fa5c997ea7b2168267caaf5cd6
bouderton <rmkhazal@gmail.com>
2024-11-16 11:35:34 -0800

    feat-project-animation

--- About-Me/src/components/Project/Project.jsx
+++ About-Me/src/components/Project/Project.jsx
@@ -1,40 +1,69 @@
 import "./Project.css";
 // import { useState } from "react";
-import github from "../../images/github.svg";
+// import github from "../../images/github.svg";
+import githubwhite from "../../images/githubwhite.svg";
 import no_image from "../../images/no_image.svg";
-import { motion } from "motion/react";
+import { motion, MotionConfig } from "motion/react";
 
 const Project = ({ link, img, title, live, text, repo }) => {
   // use live state to determine a live link to the project
   // Hover on project turns the card and github logo #3a85ff
   return (
     <>
-      <div className="project">
-        <div className="project__content">
-          <h3 className="project__title">{title}</h3>
-          {live ? (
-            <a href={link} rel="noopener noreferer" target="_blank">
-              <img src={img} className="project__img" alt="Project Image" />
-            </a>
-          ) : (
-            <img src={no_image} style={{ width: "190px", margin: "0 auto" }} />
-          )}
-          <div className="project__description">
-            <p className="project__text">{text}</p>
+      <MotionConfig transition={{ duration: 0.5, ease: "easeInOut" }}>
+        <motion.div
+          initial={{ background: "#20272d", scale: 1 }}
+          whileHover={{
+            scale: 1.05,
+            background: "#3a85ff",
+            // transition: { duration: 1, ease: "easeInOut" },
+            color: "#fbfbfc",
+          }}
+          className="project"
+        >
+          <div className="project__content">
+            <h3 className="project__title">{title}</h3>
+            {live ? (
+              <a href={link} rel="noopener noreferer" target="_blank">
+                <motion.img
+                  initial={{ scale: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                  src={img}
+                  className="project__img"
+                  alt="Project Image"
+                />
+              </a>
+            ) : (
+              <img
+                src={no_image}
+                style={{ width: "190px", margin: "0 auto" }}
+              />
+            )}
+            <div className="project__description">
+              <p className="project__text">{text}</p>
+            </div>
+            <div className="project__description-github">
+              <div className="project__github-line" />
+              <a
+                style={{ width: "30px", height: "30px" }}
+                href={repo}
+                rel="noopener noreferer"
+                target="_blank"
+              >
+                <motion.img
+                  initial={{ scale: 1 }}
+                  whileHover={{ scale: 1.3 }}
+                  transition={{ duration: 0.2 }}
+                  whileTap={{ scale: 0.5 }}
+                  src={githubwhite}
+                  className="project__github"
+                  alt="Github Link"
+                />
+              </a>
+            </div>
           </div>
-          <div className="project__description-github">
-            <div className="project__github-line"></div>
-            <a
-              style={{ width: "30px", height: "30px" }}
-              href={repo}
-              rel="noopener noreferer"
-              target="_blank"
-            >
-              <img src={github} className="project__github" alt="Github Link" />
-            </a>
-          </div>
-        </div>
-      </div>
+        </motion.div>
+      </MotionConfig>
     </>
   );
 };
