6a3e368ca342d67f0c295b0be9e76b9c359f25ed
bouderton <rmkhazal@gmail.com>
2024-11-17 20:49:03 -0800

    feat-navmobile-menu

--- About-Me/src/components/NavigationMobile/NavigationMobile.jsx
+++ About-Me/src/components/NavigationMobile/NavigationMobile.jsx
@@ -1,14 +1,14 @@
 import "../Navigation/Navigation.css";
-import { motion, useAnimate } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 import { useState } from "react";
+import { list } from "../../utils/consts";
 
 const NavigationMobile = () => {
-  //   const [isOpen, setIsOpen] = useState(false);
-  const [scope, animate] = useAnimate();
+  const [isOpen, setIsOpen] = useState(false);
 
-  //   const controls = animate([]);
-
-  const handleClick = () => {};
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <nav className="nav__mobile">
@@ -20,124 +20,142 @@ const NavigationMobile = () => {
           >
             X
           </motion.button>
-          <motion.div
-            // ref={scope}
-            // variants={{
-            //   closed: {
-            //     width: "0%",
-            //     height: "0%",
-            //   },
-            //   open: {
-            // width: "100%",
-            // height: "100vh",
-            // display: "contents",
-            // visibility: "visibile",
-            //   },
-            // }}
-            initial={{ visibility: "visible", opacity: 1 }}
-            style={{ background: "#20272d" }}
-            className="nav__mobile-menu"
-          >
-            <motion.div
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{ duration: 0.75 }}
-              className="nav__mobile-buttons"
-            >
-              <motion.button
-                initial={{ opacity: 0, scale: 1, y: 50 }}
-                animate={{
-                  y: 0,
-                  opacity: 1,
-                  transition: { delay: 0.5, duration: 0.5 },
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ visibility: "hidden", opacity: 0, scaleY: 0 }}
+                animate={{ scaleY: 1, opacity: 1, visibility: "visible" }}
+                exit={{ visibility: "hidden", opactiy: 0, scaleY: 0 }}
+                style={{
+                  background: "#20272d",
+                  borderRadius: "0 0 50px 50px",
+                  transformOrigin: "top",
                 }}
-                whileHover={{
-                  backgroundColor: "#3a85ff",
-                  borderRadius: "8px",
-                  scale: 1.2,
-                  padding: "5px",
-                  transition: {
-                    duration: 0.5,
-                    type: "spring",
-                    bounce: 0.4,
-                  },
-                }}
-                type="button"
-                className="nav__mobile-button"
+                className="nav__mobile-menu"
               >
-                Home
-              </motion.button>
-              <motion.button
-                initial={{ opacity: 0, scale: 1, y: 50 }}
-                animate={{
-                  y: 0,
-                  opacity: 1,
-                  transition: { delay: 0.75, duration: 0.5 },
-                }}
-                whileHover={{
-                  backgroundColor: "#3a85ff",
-                  borderRadius: "8px",
-                  scale: 1.2,
-                  padding: "5px",
-                  transition: {
-                    duration: 0.5,
-                    type: "spring",
-                    bounce: 0.4,
-                  },
-                }}
-                type="button"
-                className="nav__mobile-button"
-              >
-                About
-              </motion.button>
-              <motion.button
-                initial={{ opacity: 0, scale: 1, y: 50 }}
-                animate={{
-                  y: 0,
-                  opacity: 1,
-                  transition: { delay: 1, duration: 0.5 },
-                }}
-                whileHover={{
-                  backgroundColor: "#3a85ff",
-                  borderRadius: "8px",
-                  scale: 1.2,
-                  padding: "5px",
-                  transition: {
-                    duration: 0.5,
-                    type: "spring",
-                    bounce: 0.4,
-                  },
-                }}
-                type="button"
-                className="nav__mobile-button"
-              >
-                Projects
-              </motion.button>
-              <motion.button
-                initial={{ opacity: 0, scale: 1, y: 50 }}
-                animate={{
-                  y: 0,
-                  opacity: 1,
-                  transition: { delay: 1.25, duration: 0.5 },
-                }}
-                whileHover={{
-                  backgroundColor: "#3a85ff",
-                  borderRadius: "8px",
-                  scale: 1.2,
-                  padding: "5px",
-                  transition: {
-                    duration: 0.5,
-                    type: "spring",
-                    bounce: 0.4,
-                  },
-                }}
-                type="button"
-                className="nav__mobile-button"
-              >
-                Resume
-              </motion.button>
-            </motion.div>
-          </motion.div>
+                <motion.div
+                  initial="hidden"
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 0.75 }}
+                  className="nav__mobile-buttons"
+                  variants={list}
+                >
+                  <motion.button
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        delay: 0.5,
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.25,
+                      },
+                    }}
+                    exit={{ opactiy: 0, scale: 0 }}
+                    whileHover={{
+                      backgroundColor: "#3a85ff",
+                      borderRadius: "20px",
+                      scale: 1.2,
+                      transition: {
+                        duration: 0.25,
+                        type: "spring",
+                        bounce: 0.4,
+                      },
+                    }}
+                    type="button"
+                    className="nav__mobile-button"
+                  >
+                    Home
+                  </motion.button>
+                  <motion.button
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        delay: 0.75,
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.25,
+                      },
+                    }}
+                    exit={{ opactiy: 0, scale: 0 }}
+                    whileHover={{
+                      backgroundColor: "#3a85ff",
+                      borderRadius: "20px",
+                      scale: 1.2,
+                      transition: {
+                        duration: 0.5,
+                        type: "spring",
+                        bounce: 0.4,
+                      },
+                    }}
+                    type="button"
+                    className="nav__mobile-button"
+                  >
+                    About
+                  </motion.button>
+                  <motion.button
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        delay: 1,
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.25,
+                      },
+                    }}
+                    exit={{ opactiy: 0, scale: 0 }}
+                    whileHover={{
+                      backgroundColor: "#3a85ff",
+                      borderRadius: "20px",
+                      scale: 1.2,
+                      transition: {
+                        duration: 0.5,
+                        type: "spring",
+                        bounce: 0.4,
+                      },
+                    }}
+                    type="button"
+                    className="nav__mobile-button"
+                  >
+                    Projects
+                  </motion.button>
+                  <motion.button
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={{
+                      y: 0,
+                      opacity: 1,
+                      transition: {
+                        delay: 1.25,
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.25,
+                      },
+                    }}
+                    exit={{ opactiy: 0, scale: 0 }}
+                    whileHover={{
+                      backgroundColor: "#3a85ff",
+                      borderRadius: "20px",
+                      scale: 1.2,
+                      transition: {
+                        duration: 0.5,
+                        type: "spring",
+                        bounce: 0.4,
+                      },
+                    }}
+                    type="button"
+                    className="nav__mobile-button"
+                  >
+                    Resume
+                  </motion.button>
+                </motion.div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </nav>
     </>
