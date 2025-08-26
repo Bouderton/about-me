ebbc647f330663babf4f8bf262a5f6bef4651d03
bouderton <rmkhazal@gmail.com>
2025-08-26 14:06:11 -0700

    UNLV section added

--- About-Me/src/components/Next/Next.jsx
+++ About-Me/src/components/Next/Next.jsx
@@ -16,7 +16,7 @@ const Next = ({ nextRef }) => {
         <h2 className="next__title">What's Next</h2>
         <Break direction="left" />
       </motion.div>
-      <motion.div
+      {/* <motion.div
         className="next__info"
         initial={{ opacity: 0, y: 100 }}
         viewport={{ once: true }}
@@ -24,6 +24,23 @@ const Next = ({ nextRef }) => {
         whileInView={{ opacity: 1, y: 0 }}
       >
         <p className="next__info-text">We'll find out lmao</p>
+      </motion.div> */}
+      <motion.div
+        className="next__info"
+        initial={{ opacity: 0, y: 100 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+      >
+        <div className="next__unlv-container">
+          {/* motion one letter at a time! */}
+          <h2 className="next__unlv">UNLV</h2>
+          <p className="next__unlv-text">
+            I joined the class of 2029 at the University of Las Vegas, Nevada.
+            Currently exploring my major, but I have plans to stick to the
+            engineering field!
+          </p>
+        </div>
       </motion.div>
     </section>
   );
